c0a0c1df210a0c07657eb02604dcf6627c38d2e8
josecbm <jcbautista95@gmail.com>
2021-06-19 15:44:41 -0600

    disenio completado calculadora

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props {
     texto:string,
-    color?:string
+    color?:string,
+    ancho?: boolean
 } 
-export const BotonCalc = ({  texto, color  }: Props) => {
+export const BotonCalc = ({  texto, color='2D2D2D' ,ancho = false  }: Props) => {
 
     return (
-        <View style={ 
-            {
-                ...styles.boton,
-                backgroundColor: color 
-            }
-            }>
-            <Text  style={styles.botonTexto}>{ texto }</Text>
-        </View>
+        <TouchableOpacity>
+            <View style={ 
+                {
+                    ...styles.boton,
+                    backgroundColor: color ,
+                    width: ( ancho ) ? 180 : 80
+                }
+                }>
+                <Text  style={{
+                    ...styles.botonTexto,
+                    color: ( color === '#9B9B9B' ? 'black' : 'white')
+                }}>{ texto }</Text>
+            </View>
+        </TouchableOpacity>
+        
     )
 }
